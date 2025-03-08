b9ae3bafcc0eb52de726fa9500265d73967579a7
GonzaloBravo <gonzalobravo222@gmail.com>
2025-03-08 18:56:43 -0300

    Entrega del Challenge Amigo Secreto G8 One

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+===============================
+CHALLENGE AMIGO SECRETO - ALURA LATAM
+Autor: Gonzalo Ezequiel Bravo
+===============================
+*/
+
+let amigo = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert("Ingrese un nombre válido");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    renderizarAmigos();
+}
+
+function renderizarAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigo.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if(amigo.length === 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML = "";
+
+}
+
